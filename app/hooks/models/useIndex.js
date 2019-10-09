c8cf307b4b9ef451fecfad451862cc3b28f8bc4d
Pedro <zunino96@gmail.com>
2019-10-09 20:15:26 -0300

    Further dependencies fixing

--- app/hooks/models/useIndex.js
+++ app/hooks/models/useIndex.js
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
-import useSelector from './useSelector';
+import useModels from './useModels';
 import useDispatch from '../useDispatch';
 import { index } from '../../actions/modelActions';
 
-export default (model, deps) => {
-  const getIndex = useDispatch(index, [model, ...(deps || [])]);
-  useEffect(() => { getIndex(model); }, ...(deps || []));
-  const items = useSelector(model);
-  return items;
+export default (model, deps = []) => {
+  const getIndex = useDispatch(index, [model, ...deps]);
+  useEffect(() => { getIndex(model); }, deps);
+  const items = useModels(modelState => modelState.index);
+  return items || [];
 };
