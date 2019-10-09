008b721fe0169458d14a178ec280079f21a86e72
Pedro <zunino96@gmail.com>
2019-10-09 19:55:22 -0300

    Fix useIndex hook dependencies

--- app/hooks/models/useIndex.js
+++ app/hooks/models/useIndex.js
@@ -5,7 +5,7 @@ import { index } from '../../actions/modelActions';
 
 export default (model, deps) => {
   const getIndex = useDispatch(index, [model, ...(deps || [])]);
-  useEffect(() => { getIndex(model); }, deps);
+  useEffect(() => { getIndex(model); }, ...(deps || []));
   const items = useSelector(model);
   return items;
 };
